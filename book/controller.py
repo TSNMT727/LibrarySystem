557252e2ae29ead3eacc3fd9e0aa71582160a287
TSNMT727 <TSNMT727>
2025-07-20 21:58:32 +0800

    yes to yes

--- book/controller.py
+++ book/controller.py
@@ -1,15 +1,7 @@
 from book.view import (
     add_book as view_add_book,
-    main_menu,
-    user_input_main,
     user_continue,
     user_input_repeat,
-    book_added_successfully,
-    user_input_empty,
-    user_input_invalid,
-    b_attempts_maxed,
-    balik_kampung,
-    end_programm,
     display_member_bar
 )
 import book.model as model
@@ -22,46 +14,26 @@ def add_book_controller():
     while True:
         book_id, book_title, book_author = view_add_book()
         if not book_id:
-            user_input_empty()
+            print("Please enter somehting bro.")
             continue
         if model.book_exists(book_id):
             counter += 1
             if counter >= max_attempts:
-                b_attempts_maxed()
+                print("You have reached max attempts.")
                 user_choice = user_continue()
                 if user_choice == 'yes':
                     counter = 0
                     continue
                 elif user_choice == 'no':
-                    balik_kampung()
+                    print("Returning to main menu...")
                     return
             else:
                 user_input_repeat(max_attempts - counter)
                 continue
         model.add_book(book_id, book_title, book_author)
-        book_added_successfully()
+        print("Book added sucessfully")
         break
 
 def display_member_list_controller():
     display_member_bar()
     model.book_list()
-
-'''
-def main_menu_run():
-    book_model = BooksModel()
-    while True:
-        main_menu()
-        user_input = user_input_main()
-        if not user_input:
-            user_input_empty()
-            continue
-        if user_input == '1':
-            add_book_controller(book_model)
-        elif user_input == '2':
-            display_member_list_controller(book_model)
-        elif user_input == '3':
-            end_programm()
-            break
-        else:
-            user_input_invalid()
-'''
